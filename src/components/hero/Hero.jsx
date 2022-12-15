750a1df1175d99d00d161d845a2f3c94b22aa665
traso03 <robertotrasatti98@gmail.com>
2022-12-15 12:53:51 +0100

    Improve layout and color

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,23 +8,26 @@ const Hero = () => {
     <section id='hero'>
       <div className='hero--grid container'>
         <div className='hero--grid--info'>
-          <h1>
-            I help people and companies build their own{' '}
-            <span className='text-primary'>software</span>.
-          </h1>
+          <h4>Hi, i'm Roberto Trasatti.</h4>
+          <h1>Fullstack Developer Based In Rome.</h1>
           <p>
-            I'm a passionate and enthusiastic software developer from Rome,
-            Italy. I've worked as a software developer for two years in a
-            consultancy company, carrying out assignments for important clients
-            such as Aeronautica Militare, Marina Militare and Poste Italiane.
+            I'm a passionate and enthusiastic software developer. I've worked
+            for two years in a consultancy company, carrying out assignments for
+            important clients such as Aeronautica Militare, Marina Militare and
+            Poste Italiane.
           </p>
           <p>
-            I'm currently working as a Freelance and help individuals and
+            I'm currently working as a freelance and help individuals and
             companies in the development of their software.
           </p>
-          <a href='#contact' className='btn'>
-            Contact Me
-          </a>
+          <div className='hero--grid--info--cta'>
+            <a href='#contact' className='btn'>
+              My Work
+            </a>
+            <a href='#contact' className='btn btn-primary'>
+              Let's Talk
+            </a>
+          </div>
         </div>
         <div className='hero--grid--photo'>
           <img src={RobertoTrasattiPhoto} alt='Roberto Trasatti' />
